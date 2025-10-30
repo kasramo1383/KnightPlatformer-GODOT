2cc6f22572f5c83032b956c9cbd9a13ceed72114
Mahdi Abedini <m.abedini30@yahoo.com>
2025-10-30 22:06:18 +0330

    Level 1 intro, Coin HUD

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,7 +3,18 @@ extends Node
 var score = 0
 
 @onready var score_label = $ScoreLabel
+var coin_count;
 
+
+func _ready():
+	var coin_node = get_parent().get_node("Coins")
+	coin_count = coin_node.get_child_count()
+	score_label.text = "Coins: " + str(score) +"/" + str(coin_count)
+	
 func add_point():
 	score += 1
-	score_label.text = "You collected " + str(score) + " coins."
+	score_label.text = "Coins: " + str(score) +"/" + str(coin_count)
+	if score == coin_count:
+		win()
+func win():
+	print("You won")
